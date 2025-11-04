861deb229644579966e6288eebd9c8d1ab52954a
rafaelsousadevbr <rafael@sousalima.dev.br>
2025-11-03 22:07:20 -0300

    como não declarar uma variavel e a maneira correta de declarar variaveis

---
+++ src/je02_variaveis/VariaveisApp.java
@@ -0,0 +1,22 @@
+package je02_variaveis;
+
+public class VariaveisApp {
+    public static void main(String[] args) {
+        String BR = "Brasil";
+        double PI = 3.14;
+        int ESTADOS_BRASILEIRO = 27;
+        int ANO_2000 = 2000;
+
+        // Declaração inválida de variáveis
+        //int numero&um = 1; //Os únicos símbolos permitidos são _ e $
+        //int 1numero = 1; //Uma variável não pode começar com numérico
+        // int numero um = 1; //Não pode ter espaço no nome da variável
+        //int long = 1; //long faz parte das palavras reservadas da linguagem
+
+        // Declaração válida de variáveis
+        int numero$um = 1;
+        int numero1 = 1;
+        int numeroum = 1; //ou numeroUm
+        int longo = 1;
+    }
+}
