c0ebd2d7bab00102b80c5c981077279848585f18
rafaelsousadevbr <rafael@sousalima.dev.br>
2026-01-01 23:20:47 -0300

    exemplo de declaracao de tipos primitivos

---
+++ src/jr04_palavras_reservadas/TiposPrimitivos.java
@@ -0,0 +1,11 @@
+package jr04_palavras_reservadas;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int numero = 123;
+        double decimal = 123.0;
+        byte b = 127;
+        char c = 'v';
+        String nome = "Veridiana";
+    }
+}
