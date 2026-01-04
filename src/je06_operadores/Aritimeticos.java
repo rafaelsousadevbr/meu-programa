6cfe156e037e42332a4929bba4f8192e28d0e4d7
rafaelsousadevbr <rafael@sousalima.dev.br>
2026-01-03 23:39:57 -0300

    exemplo utilizando operadores aritmeticos

---
+++ src/je06_operadores/Aritimeticos.java
@@ -0,0 +1,12 @@
+package je06_operadores;
+
+public class Aritimeticos {
+    public static void main(String[] args) {
+        double soma = 11.8 + 16.4;
+        int subtracao = 234 - 211;
+        int multiplicao =  8 * 3;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado  = (10 * 7) + (20 / 4);
+    }
+}
