e92028329508ec5d9e9c120ad8f590626e4968e6
rafaelsousadevbr <rafael@sousalima.dev.br>
2026-01-03 23:31:26 -0300

    exemplo utilizando operador de atribuicao

---
+++ src/je06_operadores/Atribuicoes.java
@@ -0,0 +1,14 @@
+package je06_operadores;
+
+import java.util.Date;
+
+public class Atribuicoes {
+    public static void main(String[] args) {
+        String nome = "Rafael";
+        int idade = 38;
+        double peso = 70.08;
+        char sexo = 'M';
+        boolean doadorDeOrgao =  false;
+        Date dataDeNascimento = new Date();
+    }
+}
