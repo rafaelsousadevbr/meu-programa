7a74096e6a994d651097990ca89fc41e1e014006
rafaelsousadevbr <rafael@sousalima.dev.br>
2026-01-07 23:29:12 -0300

    exemplo utilizando JavaDoc

---
+++ src/je07_documentacao/Calculadora.java
@@ -0,0 +1,21 @@
+package je07_documentacao;
+
+/**
+ * Calculadora Simples
+ * @author Rafael Sousa
+ */
+
+public class Calculadora {
+    /**
+     * Realiza a divisão entre dois números inteiros
+     *
+     * @param dividendo Número que será dividido
+     * @param divisor Número que irá dividir o dividendo
+     * @return O resultado da divisão entre o dividendo e o divisor
+     * @author Rafael Sousa
+     * @exception java.lang.ArithmeticException em caso de passar o di
+     */
+    Integer dividir(Integer dividendo, Integer divisor){
+        return dividendo / divisor;
+    }
+}
---
+++ src/je07_documentacao/JavaDoc.java
@@ -0,0 +1,11 @@
+package je07_documentacao;
+
+public class JavaDoc {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+        Integer resultado = calculadora.dividir(30, 15);
+        System.out.println(resultado);
+    }
+
+}
